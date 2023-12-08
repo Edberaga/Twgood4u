e49ba7eeab9e8635f5aa15c32922a4d44555921d
Edberaga <edbetjonnathan@gmail.com>
2023-12-08 17:39:02 +0700

    Notify me feature

--- src/component/Notify.jsx
+++ src/component/Notify.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-toastify';
+import '../assets/scss/notify.scss';
+import { EmailAPIKey, NotifyKey } from '../data/EmailContent';
 
 const Notify = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs.sendForm(NotifyKey.service, NotifyKey.template, form.current, EmailAPIKey.publicKey)
+      .then((result) => {
+        console.log(result.text);
+        toast.success("Mail sent succesfully");
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <input type="email" name="from_email"  placeholder='Enter your email'/>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
 export default Notify
